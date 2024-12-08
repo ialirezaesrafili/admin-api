578fe60bc21e4947ab076707b6018bc856edb427
ialirezaesrafili <alirezaesrafili53@gmail.com>
2024-12-08 20:29:27 +0330

    update the category model

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -4,7 +4,7 @@ const {Schema, model} = require('mongoose');
 const categorySchema = new Schema({
     title: {type: String, required: true, unique: true},
     slug: {type: String, required: false, default: "slug-cat"},
-})
+}, {timestamps: true});
 
 const Category = model('Category', categorySchema);
 
